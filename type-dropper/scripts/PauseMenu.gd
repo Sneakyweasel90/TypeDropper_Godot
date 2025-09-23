88b85e49bae19d71fb975f2a6a3ae62fc38bcae0
Sneakyweasel90 <neil.mannion1@gmail.com>
2025-09-23 15:19:09 -0300

    pause menu buttons fixed

--- type-dropper/scripts/PauseMenu.gd
+++ type-dropper/scripts/PauseMenu.gd
@@ -2,16 +2,16 @@ extends Control
 
 func _ready():
 	visible = false
-	$VBoxContainer/ContinueBtn.pressed.connect(_on_continue_pressed)
-	$VBoxContainer/MainMenuBtn.pressed.connect(_on_main_menu_pressed)
-	
-func _on_continue_pressed():
+
+func _on_continue_btn_pressed() -> void:
+	print("Continue button pressed?")
 	visible = false
 	get_tree().paused = false
 
-func _on_main_menu_pressed():
+func _on_main_menu_btn_pressed() -> void:
+	print("Main menu button pressed?")
 	get_tree().paused = false
-	var main_menu = load("res://MainMenu.tscn").instantiate()
+	var main_menu = load("res://scenes/MainMenu.tscn").instantiate()
 	get_tree().root.add_child(main_menu)
 	if get_tree().current_scene:
 		get_tree().current_scene.queue_free()
