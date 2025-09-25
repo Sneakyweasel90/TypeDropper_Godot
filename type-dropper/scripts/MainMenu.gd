302756e84831af2b98fc52ea8bde1971336c1b59
Sneakyweasel90 <neil.mannion1@gmail.com>
2025-09-25 12:43:43 -0300

    added main menu buttons for new game modes

--- type-dropper/scripts/MainMenu.gd
+++ type-dropper/scripts/MainMenu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 @export var game_scene: PackedScene
-@onready var classic_modes = $VBoxContainer/ClassicBtn/VBoxContainer
+@onready var classic_modes = $VBoxContainer/ClassicModeBtn/ClassicModeContainer
+@onready var rapid_modes = $VBoxContainer/RapidModeBtn/RapidModeContainer
 
 func _ready():
 	classic_modes.visible = false
+	rapid_modes.visible = false
 
 func start_game(difficulty: String):
 	var scene = game_scene.instantiate()
@@ -16,6 +18,17 @@ func start_game(difficulty: String):
 
 func _on_classic_btn_pressed() -> void:
 	classic_modes.visible = !classic_modes.visible
+	rapid_modes.visible = false
 
 func _on_difficulty_btn_pressed(difficulty: String) -> void:
 	start_game(difficulty)
+
+#click off the menu
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		if classic_modes.visible and not classic_modes.get_global_rect().has_point(get_viewport().get_mouse_position()):
+			classic_modes.visible = false
+
+func _on_rapid_mode_btn_pressed() -> void:
+	rapid_modes.visible = !rapid_modes.visible
+	classic_modes.visible = false
